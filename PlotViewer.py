18f007967428196fd8e90edcb04cbe0253fe3e48
playduck <robin.prillwitz@gmail.com>
2020-05-19 12:44:12 +0200

    Update PlotViewer.py

--- PlotViewer.py
+++ PlotViewer.py
@@ -17,13 +17,14 @@ class PlotViewer(object):
         self.plt = self.win.addPlot(enableMenu=False)
 
         # self.plt.setLabel("left", units=units)
-        self.plt.setLabel("bottom", units="s")
+        self.plt.setLabel("bottom", text="")
+        self.plt.setDownsampling(auto=True)
         self.plt.addLegend(offset=(2,2))
 
         self.plt.hideButtons()
         self.plt.autoRange(padding=0.2)
 
-        self.plt.showGrid(True, True, 0.6)
+        self.plt.showGrid(True, True, 0.4)
         self.plt.vb.setLimits(minXRange=0.0001, minYRange=0.0001)
         # self.plt.vb.setAspectLocked(lock=True, ratio=1)
         self.plt.vb.setMouseMode(self.plt.vb.RectMode)
